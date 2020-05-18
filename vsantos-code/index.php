8fc5c4c1f8e4d4360571fd713110707e24872208
Viktor Denift <d-thenifts5_nc_th@outlook.com>
2020-05-18 15:39:30 +0100

    v0.0.3-56
    Signed-off-by: Viktor Denift <d-thenifts5_nc_th@outlook.com>
    #corrigi erro de responsividade da tabela em telas menores(dispositivos smartphones)
    #exportei a tabela da base de dados que estou a utilizar para buscar os dados

--- vsantos-code/index.php
+++ vsantos-code/index.php
@@ -48,98 +48,97 @@ $dados = $stmt->fetchAll(PDO::FETCH_OBJ);
 
 <!--Painel Principal-->
 <div class="container">
-    <div class="row-cols-2">
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-striped text-center my-2" style="color: yellow">
-                    <thead>
-                    <tr>
-                        <th class="border-0 vs-navbar">ANGOLA</th>
-                    </tr>
-                    <tr>
-                        <th>Províncias</th>
-                        <th>Casos Confirmados</th>
-                        <th>Activos</th>
-                        <th>Recuperados</th>
-                        <th>Óbitos</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($dados as $dado): ?>
-                        <tr>
-                            <td><?= $dado->nome ?></td>
-                            <td><?= $dado->confirmados ?></td>
-                            <td><?= $dado->activos ?></td>
-                            <td><?= $dado->recuperados ?></td>
-                            <td><?= $dado->obitos ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
+    <div class="row">
+        <div class="row-cols-1">
+            <div class="card my-4" style="background-color: #1b1e21">
+                <div class="card-body" style="background-color: #1b1e21">
+                    <div class="table-responsive">
+                        <table class="table table-striped text-center">
+                            <thead>
+                            <tr>
+                                <th>ANGOLA</th>
+                            </tr>
+                            <tr>
+                                <th>Província</th>
+                                <th>Confirmados</th>
+                                <th>Activos</th>
+                                <th>Recuperados</th>
+                                <th>Óbitos</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($dados as $dado): ?>
+                                <tr>
+                                    <td><?= $dado->nome ?></td>
+                                    <td><?= $dado->confirmados ?></td>
+                                    <td><?= $dado->activos ?></td>
+                                    <td><?= $dado->recuperados ?></td>
+                                    <td><?= $dado->obitos ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!--Painel Principal end-->
-<!--Modal Login-Form-->
-<div class="modal fade" id="login-modal">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header vs-modal-header">
-                <h4 class="modal-title vs-modal-title">
-                    <i class="fas fa-user-cog fa-lg"></i><br>
-                    Login
-                </h4>
-                <button type="button" class="close vs-modal-close" data-dismiss="modal">&times;</button>
-            </div>
-            <div class="modal-body vs-modal-body">
-                <form action="#" method="post" id="admin-login-form" class="px-3">
-                    <div class="input-group input-group-lg form-group">
-                        <div class="input-group-prepend">
+    <!--Painel Principal end-->
+    <!--Modal Login-Form-->
+    <div class="modal fade" id="login-modal">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header vs-modal-header">
+                    <h4 class="modal-title vs-modal-title">
+                        <i class="fas fa-user-cog fa-lg"></i><br>
+                        Login
+                    </h4>
+                    <button type="button" class="close vs-modal-close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body vs-modal-body">
+                    <form action="#" method="post" id="admin-login-form" class="px-3">
+                        <div class="input-group input-group-lg form-group">
+                            <div class="input-group-prepend">
                             <span class="input-group-text vs-navbar vs-modal-title">
                                 <i class="far fa-user fa-lg"></i>
                             </span>
+                            </div>
+                            <label for="u-name" class="sr-only"></label><input type="text" name="u-name" id="u-name"
+                                                                               class="form-control" placeholder="Nome"
+                                                                               required
+                                                                               autofocus>
                         </div>
-                        <label for="u-name" class="sr-only"></label><input type="text" name="u-name" id="u-name"
-                                                                           class="form-control" placeholder="Nome"
-                                                                           required
-                                                                           autofocus>
-                    </div>
-                    <div class="input-group input-group-lg form-group">
-                        <div class="input-group-prepend">
+                        <div class="input-group input-group-lg form-group">
+                            <div class="input-group-prepend">
                             <span class="input-group-text vs-navbar vs-modal-title">
                                 <i class="fas fa-key fa-lg"></i>
                             </span>
+                            </div>
+                            <label for="u-pass" class="sr-only"></label><input type="password" name="u-pass" id="u-pass"
+                                                                               class="form-control"
+                                                                               placeholder="Palavra passe" required>
                         </div>
-                        <label for="u-pass" class="sr-only"></label><input type="password" name="u-pass" id="u-pass"
-                                                                           class="form-control"
-                                                                           placeholder="Palavra passe" required>
-                    </div>
-                    <div class="form-group">
-                        <div class="forgot float-right">
-                            <a href="#" id="forgot-link">Esqueceu a sua palavra passe?</a>
+                        <div class="form-group">
+                            <div class="forgot float-right">
+                                <a href="#" id="forgot-link">Esqueceu a sua palavra passe?</a>
+                            </div>
+                            <div class="clearfix"></div>
                         </div>
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" value="Entrar" id="admin-login-form-btn"
-                               class="btn btn-lg btn-block vs-login-btn">
-                    </div>
-                </form>
+                        <div class="form-group">
+                            <input type="submit" value="Entrar" id="admin-login-form-btn"
+                                   class="btn btn-lg btn-block vs-login-btn">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
+    <!--Modal Login-Form end-->
 </div>
-<!--Modal Login-Form end-->
 
 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="assets/js/all.min.js"></script>
 <script type="text/javascript" src="assets/js/sweetalert2.min.js"></script>
-<script type="text/javascript">
-    $(document).ready(function () {
-
-    });
-</script>
 </body>
 </html>
