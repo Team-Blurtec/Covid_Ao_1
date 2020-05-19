fac8f091f51b13958590b8aeffdb650b1a9ce7b6
Viktor Denift <d-thenifts5_nc_th@outlook.com>
2020-05-19 19:32:34 +0100

    v0.1.0-10+test ##feature: painel de admin [preview]

--- vsantos-code/admin/index.php
+++ vsantos-code/admin/index.php
@@ -1,3 +1,8 @@
+<?php
+if (isset($_SESSION['admin'])) {
+    header('location: system/');
+}
+?>
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -15,70 +20,40 @@
     <link href="../assets/css/sweetalert2.css" rel="stylesheet"> <!--Estilo de alertas e fades com Sweetalert2-->
 </head>
 <body>
-<nav class="navbar navbar-expand-md vs-navbar">
-    <!--brand-->
-    <a class="navbar-brand vs-navbrand" href="./"><i class="fas fa-viruses fa-lg"></i>&nbsp;
-        &nbsp;COVID.ao</a>
-</nav>
+<nav class="navbar navbar-expand-md vs-navbar navbar-dark">
 
-<!--Modal Login-Form-->
-<div class="modal fade" id="login-modal">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header vs-modal-header">
-                <h4 class="modal-title vs-modal-title">
-                    <i class="fas fa-user-cog fa-lg"></i><br>
-                    Login
+    <a class="navbar-brand vs-navbrand" href="./"><i class="fas fa-viruses fa-lg"></i>&nbsp;
+        &nbsp;COVID.ao | Admin</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse navbar-right" id="navbarCollapse">
+        <ul class="navbar-nav ml-auto">
+            <hr class="my-3 bg-light">
+            <form action="#" method="post" id="AdminSystem-login-form" class="px-3">
+                <label for="text">
+                    <input type="text" class="form-control" placeholder="Nome de admin">
+                </label>
+                <label for="password">
+                    <input type="password" class="form-control" placeholder="Palavra passe">
+                </label>
+                <input type="submit" name="AdminSystem-login-btn" id="AdminSystem-login-btn" value="Entrar"
+                       class="btn vs-login-btn">
+            </form>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="jumbotron vs-navbar mt-5">
+                <h4 class="text-center vs-modal-title">
+                    "Com grandes poderes surgem grandes responsabilidades" - Peter Parker
                 </h4>
-                <div class="clearfix"></div>
-                <button type="button" class="close vs-modal-close" data-dismiss="modal">&times;</button>
-            </div>
-            <div class="modal-body vs-modal-body">
-                <div id="loginModalAlert"></div>
-                <form action="#" method="post" id="admin-login-form" class="px-3">
-                    <div class="input-group input-group-lg form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text vs-navbar vs-modal-title">
-                                <i class="far fa-user fa-lg"></i>
-                            </span>
-                        </div>
-                        <label for="name" class="sr-only"></label><input type="text" name="name" id="name"
-                                                                         class="form-control" placeholder="Nome"
-                                                                         required
-                                                                         autofocus>
-                    </div>
-                    <div class="input-group input-group-lg form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text vs-navbar vs-modal-title">
-                                <i class="fas fa-key fa-lg"></i>
-                            </span>
-                        </div>
-                        <label for="password" class="sr-only"></label><input type="password" name="password"
-                                                                             id="password"
-                                                                             class="form-control"
-                                                                             placeholder="Palavra passe" required>
-                    </div>
-                    <div class="form-group">
-                        <div class="float-left">
-                            <a href="#" id="registo-modal-link">
-                                <i class="fas fa-user-plus"></i>&nbsp; Nova conta
-                            </a>
-                        </div>
-                        <div class="float-right">
-                            <a href="#" id="forgot-link">Esqueceu a sua palavra passe?</a>
-                        </div>
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" value="Entrar" id="admin-login-form-btn"
-                               class="btn btn-lg btn-block vs-login-btn">
-                    </div>
-                </form>
             </div>
         </div>
     </div>
 </div>
-<!--Modal Login-Form end-->
 
 <script type="text/javascript" src="../assets/js/jquery.min.js"></script>
 <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
@@ -88,6 +63,7 @@
 <!--Scripts e funcoes-->
 <script type="text/javascript">
     $(document).ready(function () {
+        $("#login-modal").show();
 
         $("#admin-login-form-btn").click(function (e) {
             if ($("#admin-login-form")[0].checkValidity()) {
