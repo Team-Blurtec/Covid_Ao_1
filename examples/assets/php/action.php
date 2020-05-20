2cc8ec2ce54178329d83abe6c6ee84d04100d7c7
Retro <teamblurtec@gmail.com>
2020-05-20 16:40:30 +0100

    terceiro commit

--- examples/assets/php/action.php
+++ examples/assets/php/action.php
@@ -1,7 +1,29 @@
 <?php
 require_once 'auth.php';
 
+session_start();
+
 $user = new Auth();
+
+//Registrar com Ajax
+if (isset($_POST['action']) && $_POST['action'] == 'register') {
+    $name = $user->test_input($_POST['r-name']);
+    $pass = $user->test_input($_POST['r-pass']);
+
+    $hpass = password_hash($pass, PASSWORD_DEFAULT);
+
+    if ($user->existe_conta($name)) {
+        echo $user->showMessage('warning', 'Esta conta já se encontra em uso.');
+    } else {
+        if ($user->register($name, $hpass)) {
+            echo 'register';
+
+            $_SESSION['user'] = $name;
+        } else {
+            echo $user->showMessage('error', 'Algo errado ocorreu, porfavor tente novamente.');
+        }
+    }
+}
 
 //Login com Ajax
 if (isset($_POST['action']) && $_POST['action'] == 'login') {
--- examples/index.php
+++ examples/index.php
@@ -1,5 +1,94 @@
-<?php include 'conexao.php' ?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="JSKT">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>COVID.AO</title>
+    <!--Estilo manual-->
+    <link href="assets/css/vs-styles.css" rel="stylesheet">
+    <!--Estilo do bootstrap4-->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!--Outros estilos-->
+    <link href="assets/css/all.min.css"> <!--Estilo dos icones FontAwesome5-->
+    <link href="assets/css/sweetalert2.css" rel="stylesheet"> <!--Estilo de alertas e fades com Sweetalert2-->
+</head>
+<body>
+<nav class="navbar navbar-expand-md vs-navbar">
+    <!--brand-->
+    <a class="navbar-brand vs-navbrand" href="./"><i class="fas fa-viruses fa-lg"></i>&nbsp;
+        &nbsp;COVID.ao</a>
+</nav>
 
+<!--Painel Principal-->
+<div class="container">
+    <div class="row">
+        <div class="cols-2">
+            <div class="card my-4" style="background-color: #1b1e21">
+                <div class="card-body" style="background-color: #1b1e21">
+                    <div class="table-responsive" id="regions_div">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cols-1">
+            <div class="card my-4" style="background-color: #1b1e21">
+                <div class="card-body" style="background-color: #1b1e21">
+                    <div class="table-responsive" id="registros">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cols-2">
+            <div class="card my-4" style="background-color: #1b1e21">
+                <div class="card-body" style="background-color: #1b1e21">
+                    <div   id="piechart_3d" style="max-height: max-content">
+
+                    </div>
+                </div>
+            </div>
+            <div class="cols-1">
+                <div class="card my-4" style="background-color: #1b1e21">
+                    <div class="card-body" style="background-color: #1b1e21">
+                        <div  id="chart_div" style="width: 100%;height: 100%">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    </div>
+    <!--Painel Principal end-->
+</div>
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/all.min.js"></script>
+<script type="text/javascript" src="assets/js/sweetalert2.min.js"></script>
+<!--Scripts e funcoes-->
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        registros();
+
+        function registros() {
+            $.ajax({
+                url: 'assets/php/process.php',
+                method: 'post',
+                data: {action: 'apresentar'},
+                success: function (response) {
+                    console.log(response);
+                    $("#registros").html(response);
+                }
+            });
+        }
+    });
+</script>
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
@@ -41,157 +130,93 @@
         chart.draw(data, options);
     }
 </script>
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="author" content="JSKT">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>COVID.AO</title>
-    <!--Estilo manual-->
-    <link href="assets/css/vs-styles.css" rel="stylesheet">
-    <!--Estilo do bootstrap4-->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <!--Outros estilos-->
-    <link href="assets/css/all.min.css"> <!--Estilo dos icones FontAwesome5-->
-    <link href="assets/css/sweetalert2.css" rel="stylesheet"> <!--Estilo de alertas e fades com Sweetalert2-->
-</head>
-<body>
-<nav class="navbar navbar-expand-md vs-navbar navbar-dark">
-    <!--brand-->
-    <a class="navbar-brand vs-navbrand" href="./"><i class="fas fa-viruses fa-lg"></i>&nbsp;
-        &nbsp;COVID.ao</a>
-    <!--button toggler/collapsibe-->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <!--navbar links-->
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav ml-auto">
-            <hr class="my-3" style="background-color: yellow">
-            <li class="nav-item">
-                <a class="nav-link vs-navlink"
-                   href="#" id="admin-link" data-toggle="modal" data-target="#login-modal"><i
-                            class="fas fa-user-cog"></i>&nbsp;Admin</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-
-<!--Painel Principal-->
-<div class="container">
-    <div class="row">
-        <div class="col-sm">
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-striped text-center my-2" style="color: yellow">
-                        <thead>
-                        <tr>
-                            <th class="border-0 vs-navbar">ANGOLA</th>
-                        </tr>
-                        <tr>
-                            <th>Províncias</th>
-                            <th>Casos Confirmados</th>
-
-                        </tr>
-                        </thead>
-                        <tbody>
-
-
-
-                        <?php
-                        $sql="SELECT nome,confirmados FROM provincias";
-                        $buscar = mysqli_query($conexao,$sql);
-
-
-                        while ($dados = mysqli_fetch_array($buscar)){
-
-                            $nome = $dados['nome'];
-                            $confirmados =$dados['confirmados'];
-
-
-                            ?>
-                            <tr>
-                                <td><?php echo $nome?></td>
-                                <td><?php echo $confirmados?></td>
-
-                            </tr> <?php }
-                        ?>
-
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm">
-            <div class="card-body">
-                <div class="table-responsive" id="regions_div" style="width:max-content; height:max-content;border: red; border-bottom-left-radius: 16px;
-border-bottom-right-radius: 16px;border-bottom-left-radius: 16px;border-bottom-rigth-radius: 16px;background-color:red"></div> <!-- DEFINICAO DO MAPA -->
-            </div>
-        </div>
-    </div>
-</div>
-<!--Painel Principal end-->
-<!--Modal Login-Form-->
-<div class="modal fade" id="login-modal">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header vs-modal-header">
-                <h4 class="modal-title vs-modal-title">
-                    <i class="fas fa-user-cog fa-lg"></i><br>
-                    Login
-                </h4>
-                <button type="button" class="close vs-modal-close" data-dismiss="modal">&times;</button>
-            </div>
-            <div class="modal-body vs-modal-body">
-                <form action="#" method="post" id="admin-login-form" class="px-3">
-                    <div class="input-group input-group-lg form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text vs-navbar vs-modal-title">
-                                <i class="far fa-user fa-lg"></i>
-                            </span>
-                        </div>
-                        <label for="u-name" class="sr-only"></label><input type="text" name="u-name" id="u-name"
-                                                                           class="form-control" placeholder="Nome"
-                                                                           required
-                                                                           autofocus>
-                    </div>
-                    <div class="input-group input-group-lg form-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text vs-navbar vs-modal-title">
-                                <i class="fas fa-key fa-lg"></i>
-                            </span>
-                        </div>
-                        <label for="u-pass" class="sr-only"></label><input type="password" name="u-pass" id="u-pass"
-                                                                           class="form-control"
-                                                                           placeholder="Palavra passe" required>
-                    </div>
-                    <div class="form-group">
-                        <div class="forgot float-right">
-                            <a href="#" id="forgot-link">Esqueceu a sua palavra passe?</a>
-                        </div>
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" value="Entrar" id="admin-login-form-btn"
-                               class="btn btn-lg btn-block vs-login-btn">
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-<!--Modal Login-Form end-->
-
-<script type="text/javascript" src="assets/js/jquery.min.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="assets/js/all.min.js"></script>
-<script type="text/javascript" src="assets/js/sweetalert2.min.js"></script>
+<![DESENHA GRAFICO TARTE ]-->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
-    $(document).ready(function () {
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
 
-    });
+            <?php
+
+            $sql="SELECT confirmados,activos,recuperados,obitos FROM casos_registrados  ORDER BY id DESC LIMIT 1 ";
+            $buscar = mysqli_query($conexao,$sql);
+
+
+            while ($dados = mysqli_fetch_array($buscar)){
+
+            $c_activos = $dados['activos'];
+            $c_recuperados = $dados['recuperados'];
+            $c_mortes = $dados['obitos'];
+
+
+            ?>
+            ['Activos',<?php echo $c_activos?>],
+            ['Recuperados',<?php echo $c_recuperados?>],
+            ['Mortes',<?php echo $c_mortes?>]
+            <?php }
+            ?>
+
+        ]);
+
+        var options = {
+            title: 'COVID-19 PROGREÇÂO AO',
+            is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+    }
 </script>
+
+<![FIM DESENHA GRAFICO TARTE ]-->
+
+
+<![DESENHA GRAFICO DE LINHAS ]-->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Dias', 'Casos Activos', 'Casos Recuperados'],
+
+
+
+            <?php
+
+            $sql="SELECT data_casos,activos,recuperados FROM casos_registrados";
+            $buscar = mysqli_query($conexao,$sql);
+
+
+            while ($dados = mysqli_fetch_array($buscar)){
+
+            $c_data = $dados['data_casos'];
+            $c_activos = $dados['activos'];
+            $c_recuperados = $dados['recuperados'];
+
+            ?>
+            ['<?php echo $c_data?>',<?php echo $c_activos?>,<?php echo $c_recuperados?>],
+            <?php }
+            ?>
+
+
+        ]);
+
+        var options = {
+            title: 'Casos/Dias',
+            hAxis: {title: 'Dias',  titleTextStyle: {color: '#333'}},
+            vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    }
+</script>
+
+<![ FIM DESENHA GRAFICO DE LINHAS ]-->
 </body>
 </html>
