122e666da64a5d4d755a7b431e10f12c30427364
Viktor Denift <d-thenifts5_nc_th@outlook.com>
2020-05-25 15:18:22 +0100

    2.0.0-test ## - iniciando o desenvolvimento da release 2 session.php modificado - a iniciar o processo de novas sessoes dos administradores da plataforma

--- vsantos-code/assets/php/session.php
+++ vsantos-code/assets/php/session.php
@@ -1,20 +1,2 @@
 <?php
 session_start();
-
-require_once 'auth.php';
-
-$crr_user = new Auth();
-
-if (!isset($_SESSION['user'])) {
-    header('location: /');
-    die;
-}
-
-$crr_acc = $_SESSION['user'];
-
-$data = $crr_user->current_user($crr_acc);
-
-$crr_id = $data['id'];
-$crr_name = $data['nome'];
-$crr_pass = $data['passe'];
-$crr_email = $data['email'];
